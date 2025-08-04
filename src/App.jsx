c59cb4794ef742e1aa52c6c59425b9a6b6275665
titohuang3702-sketch <titohuang3702@gmail.com>
2025-08-04 11:26:26 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import OffensiveEfficiencyTracker from "./OffensiveEfficiencyTracker";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <OffensiveEfficiencyTracker />
+    </div>
+  );
+}
+
+export default App;
